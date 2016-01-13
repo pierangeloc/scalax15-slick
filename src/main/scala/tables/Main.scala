2a74afcdef038a77830d350f802dff1228ac2a26
pierangeloc <pierangeloc@gmail.com>
2016-01-13 22:23:13 +0100

    Ex1: Added year field to albums

--- src/main/scala/tables/Main.scala
+++ src/main/scala/tables/Main.scala
@@ -13,14 +13,16 @@ object Main {
   case class Album(
     artist : String,
     title  : String,
+    year   : Int,
     id     : Long = 0L)
 
   class AlbumTable(tag: Tag) extends Table[Album](tag, "albums") {
     def artist = column[String]("artist")
     def title  = column[String]("title")
+    def year   = column[Int]("year")
     def id     = column[Long]("id", O.PrimaryKey, O.AutoInc)
 
-    def * = (artist, title, id) <> (Album.tupled, Album.unapply)
+    def * = (artist, title, year, id) <> (Album.tupled, Album.unapply)
   }
 
   lazy val AlbumTable = TableQuery[AlbumTable]
@@ -34,11 +36,11 @@ object Main {
 
   val insertAlbumsAction =
     AlbumTable ++= Seq(
-      Album( "Keyboard Cat"  , "Keyboard Cat's Greatest Hits"  ), // released in 2009
-      Album( "Spice Girls"   , "Spice"                         ), // released in 1996
-      Album( "Rick Astley"   , "Whenever You Need Somebody"    ), // released in 1987
-      Album( "Manowar"       , "The Triumph of Steel"          ), // released in 1992
-      Album( "Justin Bieber" , "Believe"                       )) // released in 2013
+      Album( "Keyboard Cat"  , "Keyboard Cat's Greatest Hits"  , 2009), // released in 2009
+      Album( "Spice Girls"   , "Spice"                         , 1996), // released in 1996
+      Album( "Rick Astley"   , "Whenever You Need Somebody"    , 1987), // released in 1987
+      Album( "Manowar"       , "The Triumph of Steel"          , 1992), // released in 1992
+      Album( "Justin Bieber" , "Believe"                       , 2013)) // released in 2013
 
   val selectAlbumsAction =
     AlbumTable.result
@@ -46,9 +48,8 @@ object Main {
 
 
   // Database -----------------------------------
-
+  // use the configuration defined in application.conf relative to scalaxdb
   val db = Database.forConfig("scalaxdb")
-
 
 
   // Let's go! ----------------------------------
